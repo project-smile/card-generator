ae13af96ef91dde253840e6a328743730f94d565
Sander de Groot <sanderg@infosupport.com>
2016-12-20 11:14:42 +0100

    Added serve-templates.js which allows to render the templates so we can preview them

---
+++ site-generator/serve-templates.js
@@ -0,0 +1,27 @@
+#!/usr/bin/env node
+var mustacheExpress = require('mustache-express');
+var express = require('express');
+
+var app = express();
+
+// Register '.html' extension with The Mustache Express
+app.engine('mustache', mustacheExpress());
+
+app.set('view engine', 'mustache');
+app.set('views', __dirname + '/templates'); // you can change '/views' to '/public',
+    // but I recommend moving your templates to a directory
+    // with no outside access for security reasons
+
+app.get('/:cardId/:page', function (req, res) {
+  var card = {
+        cardId: req.params.cardId,
+        templateId: 'MSG1',
+        message: 'Welkom'
+        };
+	res.render(req.params.page, card);
+});
+
+app.use('/assets', express.static('../../project-smile.github.io/assets'));
+
+console.log('listening on 8080');
+app.listen(8080);
