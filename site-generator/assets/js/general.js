a5de734653f73313567824d9c1bf066f5c6c85c1
Sander de Groot <sanderg@infosupport.com>
2016-12-20 15:10:47 +0100

    Added disclaimer about the code quality

--- site-generator/assets/js/general.js
+++ site-generator/assets/js/general.js
@@ -1,3 +1,8 @@
+// Disclaimer
+// Please note that all the code in this project is not written with a lot of love
+// It was written two days prior to issuing the actual cards.
+// It was also purposely written with as much vanilla javascript as possible (to practise this).
+
 // google analytics code
 (function (i, s, o, g, r, a, m) {
     i['GoogleAnalyticsObject'] = r;
